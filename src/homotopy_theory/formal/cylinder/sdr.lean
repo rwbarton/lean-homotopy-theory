b845098873d99008a0c3be921e42117aa7fe3c6c
Reid Barton <rwbarton@gmail.com>
2018-06-23 09:37:37 -0400

    Strong deformation retracts.

---
+++ src/homotopy_theory/formal/cylinder/sdr.lean
@@ -0,0 +1,51 @@
+import .homotopy
+
+universe u
+
+open categories
+open categories.category
+local notation f ` ∘ `:80 g:80 := g ≫ f
+
+namespace homotopy_theory.cylinder
+
+variables {C : Type u} [cat : category C] [has_cylinder C]
+include cat
+
+-- A map j : A → X is the inclusion of a strong deformation retract if
+-- it admits a retraction r : X → A for which j ∘ r is homotopic to
+-- the identity rel j.
+structure sdr_inclusion {a x : C} (j : a ⟶ x) :=
+(r : x ⟶ a)
+(h : r ∘ j = 𝟙 a)
+(H : j ∘ r ≃ 𝟙 x rel j)
+
+def is_sdr_inclusion {a x : C} (j : a ⟶ x) : Prop := nonempty (sdr_inclusion j)
+
+lemma pushout_of_sdr_inclusion {a x a' x' : C} {j : a ⟶ x} {f : a ⟶ a'} {f' : x ⟶ x'}
+  {j' : a' ⟶ x'} (po : Is_pushout j f f' j')
+  (po' : Is_pushout (I &> j) (I &> f) (I &> f') (I &> j')) :
+  is_sdr_inclusion j → is_sdr_inclusion j' :=
+assume ⟨⟨r, h, ⟨H, Hrel⟩⟩⟩, begin
+  refine ⟨⟨po.induced (f ∘ r) (𝟙 a') (by rw [←associativity, h]; simp), by simp, _⟩⟩,
+  -- Now need to give a homotopy from j' ∘ r' (r' = the above induced
+  -- map) to 𝟙 x', rel j'. We define the homotopy using H on I +> x,
+  -- and the constant homotopy at the identity on I +> a'.
+  refine ⟨⟨po'.induced (f' ∘ H.H) (j' ∘ p @> a') _, _, _⟩, _⟩,
+  -- The above maps agree on I +> a, so we can form the induced map:
+  { unfold homotopy.is_rel at Hrel, rw [←associativity, Hrel, p_nat_assoc],
+    have : f' ∘ (j ∘ r ∘ j ∘ p @> a) = (f' ∘ j) ∘ (r ∘ j) ∘ p @> a, by simp,
+    rw [this, po.commutes, h], dsimp, simp },
+  -- The homotopy defined in this way starts at j' ∘ r' : x' → x':
+  { apply po.uniqueness; rw i_nat_assoc; conv { to_rhs, rw ←associativity }; simp;
+      rw ←associativity,
+    { rw [H.Hi₀, associativity, po.commutes] },
+    { rw [pi_components], simp } },
+  -- ... and ends at 𝟙 x':
+  { apply po.uniqueness; rw i_nat_assoc; simp; rw [←associativity],
+    { rw H.Hi₁; simp }, { rw [pi_components], simp } },
+  -- ... and is rel j:
+  { unfold homotopy.is_rel,
+    simp, congr, rw [←associativity], dsimp, simp }
+end
+
+end homotopy_theory.cylinder
