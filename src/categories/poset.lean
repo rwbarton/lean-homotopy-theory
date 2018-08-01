8d7076579ed2951c2aad364103bd4e26459dd0c5
Johan Commelin <johan@commelin.net>
2018-08-01 16:24:18 +0200

    Change namespace

--- src/categories/poset.lean
+++ src/categories/poset.lean
@@ -1,19 +1,17 @@
 import categories.category
 
-open categories
+universes u
 
-universe u
-
-namespace partial_order
+namespace categories
 
 open plift
 
-instance {X : Type u} [partial_order X] : category X :=
+instance {X : Type u} [partial_order X] : category.{u} X :=
 { Hom := λ x y, plift (x ≤ y),
-  identity := λ x, up (le_refl x),
-  compose := λ x y z f g, up (le_trans _ _ _ (down f) (down g)),
+  identity := λ x, up (partial_order.le_refl x),
+  compose := λ x y z f g, up (partial_order.le_trans _ _ _ (down f) (down g)),
   left_identity := λ x y f, up_down f,
   right_identity := λ x y f, up_down f,
   associativity := λ w x y z f g h, up_down _, }
 
-end partial_order
+end categories
